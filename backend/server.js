903d01ef23e456eefc79401c86b63725156dc9c2
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2026-03-08 23:40:13 -0400

    Added routes for my official domains for cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,18 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors()); // Allows frontend to make requests to backend
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // If you use Vite. Change to 3000 if you use Create React App.
+      "https://find-a-sponsor.vercel.app/", // Paste your exact live Vercel URL here!
+      "https://findasponsor.net",
+      "https://www.findasponsor.net",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
