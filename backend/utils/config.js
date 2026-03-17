103ece53a460332d1c40e9f907bc7d370bb2e2ca
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2026-03-17 00:36:30 -0400

    Fixed API URL issues

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,3 @@
-// src/utils/config.js
-
-// Replace the Render URL with the actual URL you got from your Render dashboard
-export const API_BASE_URL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:5000/api"
-    : "https://find-a-sponsor.onrender.com/api";
+export const API_BASE_URL = import.meta.env.PROD
+  ? "https://find-a-sponsor.onrender.com/api"
+  : "http://localhost:5000/api";
